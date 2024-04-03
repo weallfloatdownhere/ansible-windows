3f9e659873af783ac31ccd70d0e4248d0809ba8f
anon <anon@anon.com>
2024-04-03 15:21:16 -0400

    scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,15 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-ansible-playbook $SCRIPT_DIR/../playbook-demo.yml -i localhost
+cat <<EOF > "$SCRIPT_DIR/../playbook-demo.yml"
+- name: Common windows tasks executer
+  hosts: localhost
+  tasks:
+    - name: Install Demo
+      include_tasks: tasks/main.yml
+      vars: { target_product: 'demo', extras: { chocolatey: { name: thisisanextra, state: present, version: "69.0.0" } } }
+EOF
+
+ansible-playbook  -i localhost $SCRIPT_DIR/../playbook-demo.yml
+
+rm -rf "$SCRIPT_DIR/../playbook-demo.yml"
